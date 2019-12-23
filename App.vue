3c5c8d4a728603c535017388ceb47b5128de298d
king <82318179@qq.com>
2019-12-23 15:15:39 +0800

    添加 LevelJSX.js

--- App.vue
+++ App.vue
@@ -5,6 +5,8 @@
     <level :type="2">哈哈</level>
     <level :type="3">哈哈</level>
 
+    <LevelJSX :type="3">哈哈111</LevelJSX>
+
     <List :data="['香蕉','苹果','橘子']" :render="render"></List>
   </div>
 </template>
@@ -12,11 +14,13 @@
 <script>
 import Level from "./components/LevelFunction.js";
 import List from "./components/List.vue";
+import LevelJSX from "./components/LevelJSX.js";
 
 export default {
   components: {
     Level,
-    List
+    List,
+    LevelJSX
   },
   methods: {
     render(h, data) {
